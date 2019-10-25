7cce5acb4ef867622c80df0ce4313fd069ffab4b
ullia21 <ullia21@santelglobal.com>
2019-10-25 15:35:03 +0900

    191025 4강 업데이트

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Component } from 'react';
 import axios from "axios";
-import PropTypes from 'prop-types';
+import Movie from "./Movie"
+import "./App.css";
+
 
 class App extends Component {
   state = { 
-    isLoading: true
+    isLoading: true,
+    movies: []
   }
   getMovies = async () => { // async await는 동기함수를 비동기처리하기 위한 es8 문법
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     // fetch = Axios(lib) 이둘은 API 로 부터 데이터를 받아올 때 사용한다
     console.log(movies);
-    console.log(movies.data.data.movies); // data가 필요하기때문에 더 직관적으로 필요한 객체집합을 얻을수 있음
-    this.setState({ movies: movies, isLoading : false });
+    // console.log(movies.data.data.movies); 
+    // data가 필요하기때문에 더 직관적으로 필요한 객체집합을 얻을수 있음
+    this.setState({ movies, isLoading : false });
   }
   componentDidMount() {
     // setTimeout(() => {
@@ -21,10 +29,35 @@ class App extends Component {
     this.getMovies();
   }
   render() { 
-    const { isLoading } = this.state;
+    // const { isLoading, movies } = this.state;
+    const isLoading = this.state.isLoading;
+    const movies = this.state.movies;
     console.log({ isLoading });
+    console.log({ movies });
     
-    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+    return (
+      <section className="container">
+        {isLoading ? (
+           <div className="loader">
+            <span className="loader__text">Loading...</span>
+        </div>
+        ) : (
+          <div className="movies">
+            {movies.map(item => (
+              <Movie
+                key={item.id}
+                id={item.id}
+                year={item.year}
+                title={item.title}
+                summary={item.summary}
+                poster={"https://placehold.it/230x320"}
+                genres={item.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
   }
 }
  
