797327daea50a4c22cfae636e2ab2438b359d762
ullia21 <ullia21@santelglobal.com>
2019-10-25 16:20:35 +0900

    Updates

---
+++ precache-manifest.95169115689223695ae7310a43bd3671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fef25f6466dfd5521c269eda83ab0d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a6bb99d4aa6ce25baef8",
+    "url": "/movie_app/static/css/main.93d8cfa9.chunk.css"
+  },
+  {
+    "revision": "35553d95505140ba87a3",
+    "url": "/movie_app/static/js/2.e5148252.chunk.js"
+  },
+  {
+    "revision": "a6bb99d4aa6ce25baef8",
+    "url": "/movie_app/static/js/main.3a0017dd.chunk.js"
+  },
+  {
+    "revision": "5b6a134c98cd9d238fba",
+    "url": "/movie_app/static/js/runtime~main.d0c568e7.js"
+  }
+]);
